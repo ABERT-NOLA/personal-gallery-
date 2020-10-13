5fcdbec885ea97263c427da7947b64c18170c28f
Albert Obwoge <albertnyambegaa@gmail.com>
2020-10-13 08:48:27 +0300

    updates settings file

--- image_gallery/settings.py
+++ image_gallery/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,7 +117,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Imaggies)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
