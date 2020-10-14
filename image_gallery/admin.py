0f312d0c75ce2e2b2663656527eba8b130c1d8e0
Albert Obwoge <albertnyambegaa@gmail.com>
2020-10-14 13:12:18 +0300

    updated models

--- image_gallery/admin.py
+++ image_gallery/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from image_gallery.models import imggal
+from image_gallery.models import imggal,location
+
 
 admin.site.register(imggal)
+admin.site.register(location)
--- image_gallery/models.py
+++ image_gallery/models.py
@@ -4,3 +4,31 @@ class imggal(models.Model):
     imgtitle=models.CharField(max_length=100)
     imgdesc=models.CharField(max_length=500)
     image=models.ImageField(upload_to='images/')
+    location=models.ForeignKey('location',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.imgtitle
+    def save_image(self):
+        return self.save()
+
+    def delete_image(self):
+        return self.delete
+    def update_image(self):
+        return self.update
+
+class location(models.Model):
+    location = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.location
+
+    def save_location(self):
+        self.save()
+
+
+    def update_location(self):
+        self.update()
+
+
+    def delete_location(self):
+        self.delete()
